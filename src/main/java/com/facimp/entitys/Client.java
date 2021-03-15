fe7d8a4f8b6e458fe6047ee1479f8ea69dbc6fb3
Ismael Aquino <ismaellAquino@hotmail.com>
2021-03-15 12:04:36 -0300

    creating client user in database

---
+++ src/main/java/com/facimp/entitys/Client.java
@@ -0,0 +1,101 @@
+package com.facimp.entitys;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(name = "finish_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar finishDate;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private String street;
+    @Column(nullable = false)
+    private String district;
+    @Column(nullable = false)
+    private int zipCode;
+    @Column(nullable = false)
+    private String uf;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Calendar getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Calendar finishDate) {
+        this.finishDate = finishDate;
+    }
+    
+    
+}
---
+++ src/main/java/com/facimp/servlets/CreateClient.java
@@ -0,0 +1,49 @@
+package com.facimp.servlets;
+
+import com.facimp.entitys.Client;
+
+import java.io.IOException;
+import java.util.Calendar;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "CreateClient", urlPatterns = {"/CreateClient"})
+public class CreateClient extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+    
+        Client client = new Client();
+        
+        client.setName("Ismael");
+        client.setEmail("ismaellAquino@hotmail.com");
+        client.setPhone("+5599991155921");
+        client.setStreet("Rua tamôios, 39");
+        client.setUf("MA");
+        client.setZipCode(65911324);
+        client.setDistrict("Parque das estrelas");
+        client.setFinishDate(Calendar.getInstance());
+        
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("createClient");
+        EntityManager manager = factory.createEntityManager();
+        
+        manager.getTransaction().begin();
+        manager.persist(client);
+        manager.getTransaction().commit();
+        
+        log("ID da tarefa: "+ client.getId());
+        log("Criado em: "+ client.getFinishDate());
+        
+        manager.close();
+        factory.close();
+        
+        res.sendError(404);
+    }
+}
