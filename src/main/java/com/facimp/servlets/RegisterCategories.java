854cfb4434e21f7ecbcf0e36904990a41f4c7b96
Isaque Silva <isaquesilva905@gmail.com>
2021-04-18 16:52:18 -0300

    Servlet Administrador

--- src/main/java/com/facimp/servlets/RegisterCategories.java
+++ src/main/java/com/facimp/servlets/RegisterCategories.java
@@ -1,9 +1,8 @@
 
 package com.facimp.servlets;
 
-import com.facimp.entitys.Clients;
+import com.facimp.entitys.Categories;
 import java.io.IOException;
-import java.util.Calendar;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,18 +13,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name = "Carts", urlPatterns = {"/Carts"})
-public class Carts extends HttpServlet {
 
+@WebServlet(name = "RegisterCategories", urlPatterns = {"/RegisterCategories"})
+public class RegisterCategories extends HttpServlet {
+
+   
    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
         
-        Clients client =  new Clients();
+        Categories categories =  new Categories();
         
         // Definição dos parâmetros do cliente de acordo com o formulário de cadastro.
-    
+  
+      
         
         // Inicialização da Percistence Unit
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("ecommerce");
@@ -33,7 +35,7 @@ public class Carts extends HttpServlet {
         
         // Persistêcia dos dados do cliente na tabela
         manager.getTransaction().begin();
-        manager.persist(client);
+        manager.persist(categories);
         manager.getTransaction().commit();
         
         // Encerramento das conexões
