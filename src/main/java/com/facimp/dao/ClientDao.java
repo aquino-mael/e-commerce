8fab0699b639bb4e5874064cd27a2832631f6f98
Ismael Aquino <ismaellAquino@hotmail.com>
2021-05-12 01:05:39 -0300

    change administrator and client Dao to extends base and authentication dao

--- src/main/java/com/facimp/dao/ClientDao.java
+++ src/main/java/com/facimp/dao/ClientDao.java
@@ -1,18 +1,5 @@
 package com.facimp.dao;
 
 import com.facimp.entitys.Clients;
-import java.util.List;
 
-public interface ClientDao {
-    public void insertClient(Clients client);
-    
-    public Clients getClient(int id);
-    
-    public void deleteClient(int id);
-    
-    public void updateClient(Clients client);
-    
-    public Clients authenticate(String email, String password);
-    
-    public List<Clients> allClients();
-}
+public interface ClientDao extends BaseDao<Clients>, AuthenticationDao<Clients> {}
